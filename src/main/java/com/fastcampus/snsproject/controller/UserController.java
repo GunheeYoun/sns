02f0a28ccd2c0e8242e93c53ca5e3767a8be46d6
G_G <rjswkd0924@naver.com>
2023-02-11 18:40:15 +0900

    Modified : Front 연결 테스트 설정

--- src/main/java/com/fastcampus/snsproject/controller/UserController.java
+++ src/main/java/com/fastcampus/snsproject/controller/UserController.java
@@ -27,15 +27,15 @@ public class UserController {
     // TODO : implement
     @PostMapping("/join")
     public Response<UserJoinResponse> join(@RequestBody UserJoinRequest request) {
-        User user = userService.join(request.getUserName(), request.getPassword());
+        User user = userService.join(request.getName(), request.getPassword());
 //        UserJoinResponse response = UserJoinResponse.fromUser(user);
         return Response.success(UserJoinResponse.fromUser(user) /* response */);
-//        return Response.success(UserJoinResponse.fromUser(userService.join(request.getUserName(), request.getPassword())));
+//        return Response.success(UserJoinResponse.fromUser(userService.join(request.getName(), request.getPassword())));
     }
 
     @PostMapping("/login")
     public Response<UserLoginResponse> login(@RequestBody UserLoginRequest request) {
-        String token = userService.login(request.getUserName(), request.getPassword());
+        String token = userService.login(request.getName(), request.getPassword());
         return Response.success(new UserLoginResponse(token));
     }
 
